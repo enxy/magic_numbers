f9471662f97e8a96845dc36e923bcb14a1e64f39
Jola Plewa <jolanta.plewa@velis.pl>
2019-01-26 21:29:27 +0100

    Dodanie mechanizmu wczytywania plikow oraz obslugi zrzucanych wyjatkow

--- MagicNumbers.java
+++ MagicNumbers.java
@@ -1,7 +1,14 @@
 import java.util.*;
+import java.io.FileNotFoundException;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.IOException;
 
 public class MagicNumbers {
     private Map<String,int[]> signatures;
+    private static final int SIZE_OF_BUFFER = 4*1024;
+    private static final int SIZE_OF_SIGNATURE = 8;
     private static final int[] jpg = {0xff, 0xd8, 0xff, 0xe0};
     private static final int[] gif = {0x47, 0x49, 0x46, 0x38, 0x39, 0x61};
     private static final int[] png ={0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A};
@@ -21,8 +28,30 @@ public class MagicNumbers {
         return signatures.keySet();
     }
 
+    public String searchMagicNumber(String filePath){
+        byte[] buffer = new byte[SIZE_OF_BUFFER];
+        String fileType = "";
+
+        try {
+            InputStream fileStream = new FileInputStream(new File(filePath));
+            int content = fileStream.read(buffer, 0, SIZE_OF_BUFFER);
+            int a = content;
+            for (int x = content; (x < SIZE_OF_SIGNATURE) && (content > 0); x += content) {
+                content = fileStream.read(buffer, x, SIZE_OF_BUFFER - x);
+            }
+            System.out.println(a);
+            fileStream.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("Please give relative file path to method. Exception occured: " + e.getMessage());
+        } catch(IOException e) {
+            System.out.println(e.getMessage());
+        }
+
+        return fileType;
+    }
+
     public static void main(String[] args) {
         MagicNumbers mg = new MagicNumbers();
-        System.out.println(mg);
+        System.out.println(mg.searchMagicNumber("../po/README.txt"));
     }
 }
